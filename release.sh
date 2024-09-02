dfef6d40d6f49f1eac1976c8a0d7c521ac08cdf8
TennousuAthena <qingcaomc@gmail.com>
2024-09-02 22:41:27 +0800

    ci: 增加darwin_arm64架构

--- release.sh
+++ release.sh
@@ -6,11 +6,13 @@ function pack() {
 }
 
 GOOS=linux go build -ldflags "-w -s -X 'main.version=$1'" -o release/linux/elise cmd/elise/main.go
-GOOS=darwin go build -ldflags "-w -s -X 'main.version=$1'" -o release/darwin/elise cmd/elise/main.go
+GOOS=darwin_amd64 go build -ldflags "-w -s -X 'main.version=$1'" -o release/darwin_amd64/elise cmd/elise/main.go
+GOOS=darwin_arm64 go build -ldflags "-w -s -X 'main.version=$1'" -o release/darwin_arm64/elise cmd/elise/main.go
 GOOS=windows go build -ldflags "-w -s -X 'main.version=$1'" -o release/windows/elise.exe cmd/elise/main.go
 
 cd release || exit
 
 pack "linux"
-pack "darwin"
+pack "darwin_amd64"
+pack "darwin_arm64"
 pack "windows"
